36a78217f558da6979f9b7a81a8e11914b364796
lnguyen24794 <lnguyen24794@gmail.com>
2022-07-21 09:29:54 +0700

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -211,7 +211,7 @@ $(document).ready(function() {
 
     // Responsive
     responsive: true,
-    items : 5,
+    items : 1,
     itemsDesktop : [1199,4],
     itemsDesktopSmall : [980,3],
     itemsTablet: [768,2],
